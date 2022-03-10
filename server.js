445e86725e3f3a8dd919cb850668adcc7f267ba6
Joshua Reyes <reyes.j15@gmail.com>
2022-03-10 03:06:38 -0500

    phase 2 complete

--- server.js
+++ server.js
@@ -9,6 +9,35 @@ const Song = sequelize.define('song', {
         allowNull: false
     }
 });
+app.get('/api/songs', async(req, res, next) => {
+    try{
+        res.send(await Song.findAll());
+    }
+    catch(ex) {
+        next(ex);
+    }
+});
+app.post('/api/songs/:id', async(req, res, next) => {
+    try{
+        const song = await TextTrackList.findByPK(req.params.id);
+        await task.destroy();
+        res.sendStatus(201).send(await Song);
+    }
+    catch(ex) {
+        next(ex);
+    }
+});
+
+app.delete('/api/songs/:id', async(req, res, next) => {
+    try{
+        const song = await TextTrackList.findByPK(req.params.id);
+        await task.destroy();
+        res.sendStatus(204);
+    }
+    catch(ex) {
+        next(ex);
+    }
+});
 
 const start = async()=> {
     await sequelize.sync( {force: true} );
